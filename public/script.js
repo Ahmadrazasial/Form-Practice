bb551f30ecdb41893043ec75e51a40b67c1e9c0d
Ahmadrazasial <ahmadrazauch15@gmail.com>
2026-01-25 21:33:32 +0500

    added phone number differently

--- public/script.js
+++ public/script.js
@@ -1,196 +1,82 @@
-const countryPhoneCodes = [
-    { name: "Afghanistan", dial_code: "+93" },
-    { name: "Albania", dial_code: "+355" },
-    { name: "Algeria", dial_code: "+213" },
-    { name: "American Samoa", dial_code: "+1-684" },
-    { name: "Andorra", dial_code: "+376" },
-    { name: "Angola", dial_code: "+244" },
-    { name: "Anguilla", dial_code: "+1-264" },
-    { name: "Antarctica", dial_code: "+672" },
-    { name: "Antigua and Barbuda", dial_code: "+1-268" },
-    { name: "Argentina", dial_code: "+54" },
-    { name: "Armenia", dial_code: "+374" },
-    { name: "Aruba", dial_code: "+297" },
-    { name: "Australia", dial_code: "+61" },
-    { name: "Austria", dial_code: "+43" },
-    { name: "Azerbaijan", dial_code: "+994" },
-    { name: "Bahamas", dial_code: "+1-242" },
-    { name: "Bahrain", dial_code: "+973" },
-    { name: "Bangladesh", dial_code: "+880" },
-    { name: "Barbados", dial_code: "+1-246" },
-    { name: "Belarus", dial_code: "+375" },
-    { name: "Belgium", dial_code: "+32" },
-    { name: "Belize", dial_code: "+501" },
-    { name: "Benin", dial_code: "+229" },
-    { name: "Bermuda", dial_code: "+1-441" },
-    { name: "Bhutan", dial_code: "+975" },
-    { name: "Bolivia", dial_code: "+591" },
-    { name: "Bosnia and Herzegovina", dial_code: "+387" },
-    { name: "Botswana", dial_code: "+267" },
-    { name: "Brazil", dial_code: "+55" },
-    { name: "British Virgin Islands", dial_code: "+1-284" },
-    { name: "Brunei", dial_code: "+673" },
-    { name: "Bulgaria", dial_code: "+359" },
-    { name: "Burkina Faso", dial_code: "+226" },
-    { name: "Burundi", dial_code: "+257" },
-    { name: "Cambodia", dial_code: "+855" },
-    { name: "Cameroon", dial_code: "+237" },
-    { name: "Canada", dial_code: "+1" },
-    { name: "Cape Verde", dial_code: "+238" },
-    { name: "Cayman Islands", dial_code: "+1-345" },
-    { name: "Central African Republic", dial_code: "+236" },
-    { name: "Chad", dial_code: "+235" },
-    { name: "Chile", dial_code: "+56" },
-    { name: "China", dial_code: "+86" },
-    { name: "Colombia", dial_code: "+57" },
-    { name: "Comoros", dial_code: "+269" },
-    { name: "Congo (Brazzaville)", dial_code: "+242" },
-    { name: "Congo (Kinshasa)", dial_code: "+243" },
-    { name: "Costa Rica", dial_code: "+506" },
-    { name: "Croatia", dial_code: "+385" },
-    { name: "Cuba", dial_code: "+53" },
-    { name: "Cyprus", dial_code: "+357" },
-    { name: "Czech Republic", dial_code: "+420" },
-    { name: "Denmark", dial_code: "+45" },
-    { name: "Djibouti", dial_code: "+253" },
-    { name: "Dominica", dial_code: "+1-767" },
-    { name: "Dominican Republic", dial_code: "+1-809" },
-    { name: "Ecuador", dial_code: "+593" },
-    { name: "Egypt", dial_code: "+20" },
-    { name: "El Salvador", dial_code: "+503" },
-    { name: "Equatorial Guinea", dial_code: "+240" },
-    { name: "Eritrea", dial_code: "+291" },
-    { name: "Estonia", dial_code: "+372" },
-    { name: "Eswatini", dial_code: "+268" },
-    { name: "Ethiopia", dial_code: "+251" },
-    { name: "Fiji", dial_code: "+679" },
-    { name: "Finland", dial_code: "+358" },
-    { name: "France", dial_code: "+33" },
-    { name: "Gabon", dial_code: "+241" },
-    { name: "Gambia", dial_code: "+220" },
-    { name: "Georgia", dial_code: "+995" },
-    { name: "Germany", dial_code: "+49" },
-    { name: "Ghana", dial_code: "+233" },
-    { name: "Greece", dial_code: "+30" },
-    { name: "Greenland", dial_code: "+299" },
-    { name: "Grenada", dial_code: "+1-473" },
-    { name: "Guatemala", dial_code: "+502" },
-    { name: "Guinea", dial_code: "+224" },
-    { name: "Guyana", dial_code: "+592" },
-    { name: "Haiti", dial_code: "+509" },
-    { name: "Honduras", dial_code: "+504" },
-    { name: "Hong Kong", dial_code: "+852" },
-    { name: "Hungary", dial_code: "+36" },
-    { name: "Iceland", dial_code: "+354" },
-    { name: "India", dial_code: "+91" },
-    { name: "Indonesia", dial_code: "+62" },
-    { name: "Iran", dial_code: "+98" },
-    { name: "Iraq", dial_code: "+964" },
-    { name: "Ireland", dial_code: "+353" },
-    { name: "Israel", dial_code: "+972" },
-    { name: "Italy", dial_code: "+39" },
-    { name: "Jamaica", dial_code: "+1-876" },
-    { name: "Japan", dial_code: "+81" },
-    { name: "Jordan", dial_code: "+962" },
-    { name: "Kazakhstan", dial_code: "+7" },
-    { name: "Kenya", dial_code: "+254" },
-    { name: "Kuwait", dial_code: "+965" },
-    { name: "Kyrgyzstan", dial_code: "+996" },
-    { name: "Laos", dial_code: "+856" },
-    { name: "Latvia", dial_code: "+371" },
-    { name: "Lebanon", dial_code: "+961" },
-    { name: "Lesotho", dial_code: "+266" },
-    { name: "Liberia", dial_code: "+231" },
-    { name: "Libya", dial_code: "+218" },
-    { name: "Lithuania", dial_code: "+370" },
-    { name: "Luxembourg", dial_code: "+352" },
-    { name: "Malaysia", dial_code: "+60" },
-    { name: "Maldives", dial_code: "+960" },
-    { name: "Mexico", dial_code: "+52" },
-    { name: "Mongolia", dial_code: "+976" },
-    { name: "Morocco", dial_code: "+212" },
-    { name: "Nepal", dial_code: "+977" },
-    { name: "Netherlands", dial_code: "+31" },
-    { name: "New Zealand", dial_code: "+64" },
-    { name: "Nigeria", dial_code: "+234" },
-    { name: "North Korea", dial_code: "+850" },
-    { name: "Norway", dial_code: "+47" },
-    { name: "Oman", dial_code: "+968" },
-    { name: "Pakistan", dial_code: "+92" },
-    { name: "Philippines", dial_code: "+63" },
-    { name: "Poland", dial_code: "+48" },
-    { name: "Portugal", dial_code: "+351" },
-    { name: "Qatar", dial_code: "+974" },
-    { name: "Romania", dial_code: "+40" },
-    { name: "Russia", dial_code: "+7" },
-    { name: "Saudi Arabia", dial_code: "+966" },
-    { name: "Singapore", dial_code: "+65" },
-    { name: "South Africa", dial_code: "+27" },
-    { name: "South Korea", dial_code: "+82" },
-    { name: "Spain", dial_code: "+34" },
-    { name: "Sri Lanka", dial_code: "+94" },
-    { name: "Sweden", dial_code: "+46" },
-    { name: "Switzerland", dial_code: "+41" },
-    { name: "Thailand", dial_code: "+66" },
-    { name: "Turkey", dial_code: "+90" },
-    { name: "UAE", dial_code: "+971" },
-    { name: "United Kingdom", dial_code: "+44" },
-    { name: "United States", dial_code: "+1" },
-    { name: "Vietnam", dial_code: "+84" },
-    { name: "Yemen", dial_code: "+967" },
-    { name: "Zambia", dial_code: "+260" },
-    { name: "Zimbabwe", dial_code: "+263" }
-];
+
 
 const Firstname = document.getElementById("first")
 const Lastname = document.getElementById("last")
-const Countrycode = document.getElementById("code");
+const userCountry = document.getElementById("userCountry");
 const Phone = document.getElementById("phone")
 const Email = document.getElementById("email")
 const Password = document.getElementById("password")
 const Confirm = document.getElementById("confirm")
 const formsSec = document.getElementById("formSec");
 const errSpans = document.querySelectorAll(".errSpan");
-const codeField = document.querySelector(".wraper")
+const countryList = document.getElementById("countries");
+const countries = document.querySelectorAll(".country");
+const userFlag = document.getElementById("countryFlag")
+const flagImg = document.getElementById("countryFlag");
 
 
+(async function () {
+    const API = await fetch("https://restcountries.com/v3.1/all?fields=name,flags,idd,cca2")
+    const apiData = await API.json()
+    apiData.forEach(country => {
+        const div = document.createElement("div");
+        div.className = "country";
+        div.id = "country";
+        div.dataset.iso = country.cca2
+        div.dataset.name = country.name.common;
+        div.dataset.dial = country.idd.root + (country.idd.suffixes?.[0] || "");
+        div.dataset.flag = country.flags.svg;
+        const countryflag = document.createElement("img")
+        countryflag.className = "countryFlag";
+        countryflag.src = country.flags.svg;
+        const countryName = document.createElement("span")
+        countryName.className = "countryName";
+        countryName.textContent = country.name.common;
+        countryName.dataset = country.name.common;
+        const countryDial = document.createElement("span")
+        countryDial.className = "countryDial";
+        countryDial.textContent = country.idd.root + (country.idd.suffixes?.[0] || "");
+        div.append(countryflag, countryName, countryDial)
+        countryList.append(div)
+        // console.log(country.idd)
+    })
+})()
 
-countryPhoneCodes.forEach(code => {
-    const option = document.createElement("option")
-    option.classList.add("option")
-    option.value = code.dial_code;
-    option.textContent = `${code.name} ${code.dial_code}`;
-    Countrycode.append(option)
+userCountry.addEventListener("click",(e)=>{
+    countryList.classList.replace("h-0","h-[60vh]")
+    countryList.classList.replace("-top-0","-top-56")
+    countryList.classList.remove("opacity-0")
 })
 
-
-
-const codeDisplay = document.getElementById("codeDisplay");
-
-Countrycode.addEventListener("change", () => {
-    // const selected = Countrycode.selectedOptions[0];  
-    const selected = Countrycode.querySelector('option:checked');
-    if (selected) {
-        codeDisplay.textContent = selected.value + " ↓"
-        Countrycode.blur()
+window.addEventListener("click",(e)=>{
+    if(!countryList.contains(e.target) && !userCountry.contains(e.target)){
+        countryList.classList.replace("h-[60vh]","h-0")
+    countryList.classList.replace("-top-56","-top-0")
+    countryList.classList.add("opacity-0")
     }
 })
-Countrycode.addEventListener("focus", () => {
-    Countrycode.value = "";
-})
-Countrycode.addEventListener("keydown", () => {
-    codeDisplay.textContent = "";
-})
-// const options = Countrycode.options
-// console.log(options)
 
-// Array.from(options).forEach((option) => {
-//     option.addEventListener("", () => {
-//         codeDisplay.textContent = ""
-//     })
-// })
+countryList.addEventListener("click",(e)=>{
+    const item = e.target.closest(".country")
+    if(!item) return;
+    userFlag.src = item.dataset.flag
+    userFlag.dataset.iso = item.dataset.iso
+    const selectedDial = item.dataset.dial;
+    const local = Phone.value.replace(/^\+\d+\s*/, "");
+    Phone.value = `${item.dataset.dial} ${local}`;
+    Phone.value = selectedDial + " " + local;
+    countryList.classList.replace("h-[60vh]","h-0")
+    countryList.classList.replace("-top-56","-top-0")
+    countryList.classList.add("opacity-0")
+})
 
-const requiredFields = [Firstname, Lastname, Countrycode, Phone, Email, Password, Confirm];
+// async function setDial(country_code) {
+//     const ip = await fetch('https://ipwho.is')
+//     const res = await ip.json();
+//     console.log(res)
+// }
+
+const requiredFields = [Firstname, Lastname, userCountry, Phone, Email, Password, Confirm];
 
 function showErr(input, message) {
     const error = document.querySelector(`.errSpan[data-for="${input.id}"]`);
@@ -229,12 +115,14 @@ function validateln() {
 }
 
 function validatecode() {
-    if (Countrycode.value.trim() === "") {
-        showErr(Countrycode, "This field is required");
+    const flagImg = document.getElementById("countryFlag");
+    console.log(flagImg);
+    if (flagImg.dataset.iso === " ") {
+        showErr(flagImg.parentElement, "This field is required");
         return false;
     }
     else {
-        clearErr(Countrycode);
+        clearErr(flagImg.parentElement);
         return true;
     }
 }
